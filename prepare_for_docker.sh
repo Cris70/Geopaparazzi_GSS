358bf0c4c9783fa21d9d150ce06f424b0e2e400b
Cristiano Guadagnino <criguada+bitbucket@gmail.com>
2026-02-12 16:15:53 +0100

    Update to avoid the missing images in the visualization of the imagegrid control on the server side (flutter frontend).

--- prepare_for_docker.sh
+++ prepare_for_docker.sh
@@ -1,10 +1,13 @@
 set -x
 
 cd server_backend_django
-source env/bin/activate
+source .venv/bin/activate
 
 rm -rf ./static
 mkdir static
+
+
+read -p "Press Enter to continue..."
 
 
 cd gss
@@ -15,10 +18,18 @@ if [ -f ./config.sh ]; then
 fi
 ./manage.py collectstatic --noinput
 
+
+read -p "Press Enter to continue..."
+
+
 # go build the frontend
 cd ../../server_frontend_flutter/
 flutter clean
 flutter build web
+
+
+read -p "Press Enter to continue..."
+
 
 # copy the frontend to the static folder
 cp -rv build/web/* ../server_backend_django/static/
