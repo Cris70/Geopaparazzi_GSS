d937fd95a4fa38d7906a174f7dd9e1775c571274
Cristiano Guadagnino <criguada+bitbucket@gmail.com>
2026-02-25 23:45:10 +0100

    Changed the way the imagegrid is shown server-side: if IMAGEGRID_SHOW_ALL is false, only the chosen picture is shown. Added dependentcombo widget in smashlibs submodule.

--- prepare_for_docker.sh
+++ prepare_for_docker.sh
@@ -26,7 +26,8 @@ read -p "Press Enter to continue..."
 cd ../../server_frontend_flutter/
 flutter clean
 GSS_GIT_TAG=$(grep -E '^version = "' ../server_backend_django/pyproject.toml | cut -d '"' -f2)
-flutter build web --dart-define=GSS_GIT_TAG="$GSS_GIT_TAG"
+IMAGEGRID_SHOW_ALL=${GSS_IMAGEGRID_SHOW_ALL:-true}
+flutter build web --dart-define=GSS_GIT_TAG="$GSS_GIT_TAG" --dart-define=IMAGEGRID_SHOW_ALL="$IMAGEGRID_SHOW_ALL"
 
 
 read -p "Press Enter to continue..."
--- server_frontend_flutter/build_deploy.sh
+++ server_frontend_flutter/build_deploy.sh
@@ -2,7 +2,8 @@ set -x
 
 flutter clean
 GSS_GIT_TAG=$(grep -E '^version = "' ../server_backend_django/pyproject.toml | cut -d '"' -f2)
-flutter build web --dart-define=GSS_GIT_TAG="$GSS_GIT_TAG"
+IMAGEGRID_SHOW_ALL=${GSS_IMAGEGRID_SHOW_ALL:-true}
+flutter build web --dart-define=GSS_GIT_TAG="$GSS_GIT_TAG" --dart-define=IMAGEGRID_SHOW_ALL="$IMAGEGRID_SHOW_ALL"
 
 rm -rf ../server_backend_django/static/assets
 rm -rf ../server_backend_django/static/canvaskit
